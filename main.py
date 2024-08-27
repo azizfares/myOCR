deb1f627dc12cbeede3be9b2a19d03fd09ce9b40
rostamzad1922 <rostamzad1922@gmail.com>
2024-08-28 01:16:03 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import os
+import time
+for i in range(11*3600):
+	if i % 100 == 0:
+		print(i)
+	time.sleep(1)
+for i in range (3600):
+	if i % 100 == 0:
+		print("!!!	Stop this CS")
